a7e9780b2053b79d1a94c47bd2fb5cd7a8d1c560
kazuyoshi.maki <kazuyosi.maki@gmail.com>
2020-07-14 08:00:51 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-    root to: 'toppages#index'
+  root to: 'toppages#index'
     
-    resources :tasks
+ 
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+    
     get 'signup', to: 'users#new'
     resources :users, only: [:index, :show, :new, :create]
 end
