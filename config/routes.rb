fbe40a3107b25be8130cb5241ac0b264f143ed08
kazuyoshi.maki <kazuyosi.maki@gmail.com>
2020-07-14 08:59:40 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :tasks
   root to: 'toppages#index'
-    
- 
   
+    
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
