d1236abfac15cd6c59a08b6e586e1e88605fe2ac
kazuyoshi.maki <kazuyosi.maki@gmail.com>
2020-07-18 13:43:22 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only:[:show, :edit, :update, :destroy]
-    before_action :require_user_logged_in, only: [:index, :show, :edit, :new]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:edit, :destroy,]
     
     def user
     end    
@@ -24,6 +25,7 @@ class TasksController < ApplicationController
             flash[:success] = 'Task が正常に投稿されました'
             redirect_to @task
         else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'Task が投稿されませんでした'
             render :new
         end    
@@ -58,5 +60,12 @@ class TasksController < ApplicationController
     
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end
 end
